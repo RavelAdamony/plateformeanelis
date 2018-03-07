e14dbdefe213d925ca6593e6db38e6e20d33df10
Petit Pwney <Petit Pwney>
2018-03-07 14:56:54 +0100

    Added most of what is needed for Prod push

--- src/Admin/MailingBundle/Controller/CRUDController.php
+++ src/Admin/MailingBundle/Controller/CRUDController.php
@@ -9,20 +9,22 @@ use Sonata\AdminBundle\Controller\CRUDController as Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class CRUDController extends Controller
-{
-	//Identifiants de la BD
-	const DB_SERVERNAME = "localhost";
-	const DB_USERNAME = "root";
-	const DB_PASSWORD = "root";
-	const DB_DBNAME = "panelis";
-	
+{	
+	/**
+     * Récupère le Mailjet_ID d'une liste de diffusion de la BD.
+     *
+     * @param int $id  L'ID d'une liste dans la BD
+     *
+     * @return string $list_id L'ID mailjet d'une liste de diffusion. 0 si elle n'est pas trouvée.
+	 *
+     */
 	private function getDBListID($id){
 		
 		//La valeur de retour. 0 si la liste n'est pas trouvée
 		$list_id = 0;
 		
 		// Connexion à la BD
-		$conn = new \mysqli(self::DB_SERVERNAME, self::DB_USERNAME, self::DB_PASSWORD, self::DB_DBNAME);
+		$conn = new \mysqli(getenv('MYSQL_ADDON_HOST'), getenv('MYSQL_ADDON_USER'), getenv("MYSQL_ADDON_PASSWORD"), getenv('MYSQL_ADDON_DB'));
 		if ($conn->connect_error) {
 			die("Connection failed: " . $conn->connect_error);
 		}
@@ -46,20 +48,28 @@ class CRUDController extends Controller
 		return $list_id;	
 	}
 	
+	/**
+     * Récupère tous les utilisateurs de la BD.
+     *
+     * @param string $list_id  L'ID mailjet d'une liste de diffusion
+     *
+     * @return array $users La liste des utilisateurs
+	 *
+     */
 	private function getDBUsers($list_id){
 		
 		//La valeur de retour
 		$users=array();
 		
 		// Connexion à la BD
-		$conn = new \mysqli(self::DB_SERVERNAME, self::DB_USERNAME, self::DB_PASSWORD, self::DB_DBNAME);
+		$conn = new \mysqli(getenv('MYSQL_ADDON_HOST'), getenv('MYSQL_ADDON_USER'), getenv("MYSQL_ADDON_PASSWORD"), getenv('MYSQL_ADDON_DB'));
 		if ($conn->connect_error) {
 			die("Connection failed: " . $conn->connect_error);
 		}
 		
 		else{
 			//Requête SQL (Récupération des utilisateurs de la BD)
-			$sql = "SELECT username, email FROM user WHERE id IN (SELECT user_id from subscriber WHERE newsletter_id=(SELECT id FROM newsletter WHERE mailjet_id=".$list_id.")) LIMIT 3";
+			$sql = "SELECT username, email FROM user WHERE id IN (SELECT user_id from subscriber WHERE newsletter_id=(SELECT id FROM newsletter WHERE mailjet_id=".$list_id."))";
 			$result = $conn->query($sql);
 			
 			// Récupération des utilisateurs à ajouter
@@ -79,17 +89,46 @@ class CRUDController extends Controller
 		return $users;
 	}
 	
+	/**
+     * Récupère tous les utilisateurs d'une liste de Mailjet
+     *
+     * @param string $list_id  L'ID mailjet d'une liste de diffusion
+     *
+     * @return array|int $result La liste des utilisateurs. 0 S'il y'a eu une erreur lors du contact avec Mailjet. 1 si la liste n'a pas été trouvée.
+	 *
+     */
 	private function mailjetGetAllUsers($list_id){
 		
 		//Récupération de tous les contacts de la liste Mailjet
 		$mj = new \Mailjet\Client(getenv('MJ_PUBLIC_KEY'), getenv('MJ_PRIVATE_KEY'));
-		$result = getenv('MJ_PUBLIC_KEY');
+		$result = 0;
+		
+		//On récupère tous les contacts de la liste
 		$response = $mj->get(Resources::$Contact, ['contactslist' => $list_id]);
 		if ($response->success())
 			$result = $response->getData();
+			
+		//Si ça n'a pas marché, on vérifie que la liste existe
+		else{			
+			$response = $mj->get(Resources::$Listrecipient, ['contactslist' => $list_id]);
+			
+			//Si elle existe, on renvoie 1 au lieu de 0
+			if (!empty($response->getData())){
+				$result = 1;
+			}
+		}
 		return $result;   
 	}
 	
+	/**
+     * Exporte une liste d'utilisateurs vers une liste Mailjet
+     *
+     * @param string $list_id  L'ID mailjet d'une liste de diffusion
+	 * @param array $users La liste des utilisateurs à exporter
+     *
+     * @return int 0
+	 *
+     */
 	private function mailjetExportList($list_id, $users){
 	
 		//Ajout des contacts à la liste
@@ -105,11 +144,19 @@ class CRUDController extends Controller
 		];
 	
 		//On effectue les modifications sur la liste Mailjet et on retourne un message indiquant un succès
-		$response = $mj->post(Resources::$ContactManagemanycontacts, ['body' => $body]);
-		$response->success() && var_dump($response->getData());
+		$mj->post(Resources::$ContactManagemanycontacts, ['body' => $body]);
 		return 0;
 	}
 	
+	/**
+     * Supprime les utilisateurs une liste Mailjet
+     *
+     * @param string $list_id  L'ID mailjet d'une liste de diffusion
+	 * @param array $users La liste des utilisateurs à supprimer
+     *
+     * @return int 0
+	 *
+     */
 	private function mailjetDeleteList($list_id, $users){
 	
 		//Ajout des contacts à la liste
@@ -125,11 +172,17 @@ class CRUDController extends Controller
 		];
 	
 		//On effectue les modifications sur la liste Mailjet et on retourne un message d'erreur
-		$response = $mj->post(Resources::$ContactManagemanycontacts, ['body' => $body]);
-		$response->success() && var_dump($response->getData());
+		$mj->post(Resources::$ContactManagemanycontacts, ['body' => $body]);
 		return 0;
 	}
 	
+	/**
+     * Action relative au clic sur le bouton "Maijet Export"
+     *
+     * @param int $id  L'ID de la ligne cliquée
+	 *
+	 * @return RedirectResponse $url L'URL permettant de revenir sur la page de la liste des newsletters
+     */
     public function mailjetexportAction($id)
     {
 		
@@ -143,24 +196,31 @@ class CRUDController extends Controller
 		$list_id = self::getDBListID($id);
 		
 		//Si la liste existe
-		if ($list_id != 0){
+		if ($list_id != '0'){
 			
-			//Vidage de la liste Mailjet
+			//Récupération de tous les utilisateurs de la liste Mailjet concernée
 			$users = self::mailjetGetAllUsers($list_id);
 			if ($users != 0){
-				self::mailjetDeleteList($list_id, $users);
-			
-				// Export de la liste des contacts de la BD vers Mailjet
-				$users = self::getDBUsers($list_id);
-				self::mailjetExportList($list_id, $users);
+				if ($users != 1){
+					self::mailjetDeleteList($list_id, $users);
 				
-				//Message indiquant un succès
-				$this->addFlash('sonata_flash_success', 'Export réussi');
+					// Export de la liste des contacts de la BD vers Mailjet
+					$users = self::getDBUsers($list_id);
+					self::mailjetExportList($list_id, $users);
+						
+						//Message indiquant un succès
+						$this->addFlash('sonata_flash_success', 'Export réussi');
+				}
+				
+				//Si la liste à supprimer n'a pas été trouvée sur Mailjet, afficher un message d'erreur
+				else{
+					$this->addFlash('sonata_flash_error', 'Aucune liste mailjet trouvée avec ID = '.$list_id);
+				}
 			}
 			
-			//Si le vidage a échoué, la liste n'existe pas
+			//Si on n'a pas réussi à accéder à Mailjet, afficher un message d'erreur
 			else{
-				$this->addFlash('sonata_flash_error', $users);
+				$this->addFlash('sonata_flash_error', 'Impossible de joindre Mailjet');
 			}
 		}
 		
@@ -173,6 +233,13 @@ class CRUDController extends Controller
         return new RedirectResponse($this->admin->generateUrl('list'));
     }
 
+	/**
+     * Action relative à l'ajout d'un utilisateur"
+     *
+     * @param int $id  L'ID de la ligne cliquée
+	 *
+	 * @return RedirectResponse $url L'URL permettant de revenir sur la page de la liste des newsletters
+     */
     public function addUserListAction($id)
     {
         $object = $this->admin->getSubject();
